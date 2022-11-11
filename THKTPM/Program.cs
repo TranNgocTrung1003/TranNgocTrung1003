7fb9f95121789ec0e5c79c6591db3d8bb5ca6217
TranNgocTrung1003 <hahatrung4@gmail.com>
2022-11-11 22:04:51 +0700

    update From1

--- THKTPM/Program.cs
+++ THKTPM/Program.cs
@@ -16,7 +16,7 @@ namespace THKTPM
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Tính());
         }
     }
 }
