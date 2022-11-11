aee001533fdf7e796117c00e8f668f92fd7b57c1
TranNgocTrung1003 <hahatrung4@gmail.com>
2022-11-11 22:30:26 +0700

    Nút cộng

--- THKTPM/Form1.cs
+++ THKTPM/Form1.cs
@@ -16,5 +16,11 @@ namespace THKTPM
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a, b, ketqua;
+           
+        }
     }
 }
